3c15c7e88721694f7026a8e0fc72e9c71b48b7aa
stephen <stephen@pvp-live.com>
2014-10-23 15:41:21 -0500

    wordpress loop

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php get_header(); ?>
 
+<!-- main -->
 <main role="main">
-  index
+  <section>
+    <?php if ( have_posts() ) : ?>
+      <?php while ( have_posts() ) : the_post(); ?>    
+      <article>
+        <header>
+          <h2><?php the_title(); ?></h2>
+          <time pubdate timedate="<?php get_the_time(); ?> "><?php get_the_time(); ?> </time>
+        </header>
+        <p><?php the_content(); ?></p>
+      </article>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </section>
 </main>
+<!-- /main -->
 
 <?php get_footer(); ?>
