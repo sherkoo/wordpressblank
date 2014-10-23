380661e4bf4e436708523428404cd28fe34da4b7
stephen <stephen@pvp-live.com>
2014-10-23 16:10:47 -0500

    menu support

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php 
 
-  // functions
+// functions
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'primary-menu' => __( 'Primary Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_my_menus' );
 
 ?>
