e48feb16b21d908ade12485dcbb7339b982ffb86
stephen <stephen@pvp-live.com>
2014-10-28 09:16:30 -0500

    cleaned up <head>

--- header.php
+++ header.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en-US" prefix="og: http://ogp.me/ns#"> <!--<![endif]-->
 <head>
+  <!-- Title -->
   <title><?php the_title(); ?> - <?php echo get_bloginfo('name'); ?></title>
-  <?php wp_head(); ?>
 
-  <!-- meta -->
+  <!-- Meta -->
   <meta name="description" content="<?php meta_description(); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="<?php bloginfo('charset'); ?>">
+
+  <!-- Mobile -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Favicon -->
+  <link href="<?php echo get_template_directory_uri(); ?>assets/img/icons/favicon.ico" rel="shortcut icon">
+  <link href="<?php echo get_template_directory_uri(); ?>assets/img/icons/favicon.png" rel="apple-touch-icon-precomposed">
   
+  <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
