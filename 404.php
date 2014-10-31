befa7ab86b0e030ff9505d495706687d0535aa40
stephen <stephen@pvp-live.com>
2014-10-31 11:12:16 -0500

    update 404.php

--- 404.php
+++ 404.php
@@ -1,13 +1,7 @@
 <?php get_header(); ?>
 
-<!-- section -->
-<section>
-  <article>
-    <h2><?php _e('Sorry Page Not Found', 'wordpressblank'); ?></h2>
-    <a href="<?php echo home_url(); ?>"><?php _e('Return Home?', 'wordpressblank'); ?></a>
-  </article>
-</section>
-<!-- /section -->
+<h2><?php _e('Sorry Page Not Found', 'wordpressblank'); ?></h2>
+<a href="<?php echo home_url(); ?>"><?php _e('Return Home?', 'wordpressblank'); ?></a>
 
 <?php get_sidebar(); ?>
 
