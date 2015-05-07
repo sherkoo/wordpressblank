2001d96b63afc0fc33b9aa0604172ca36413e460
Stephen <stephenherko@gmail.com>
2015-05-06 23:28:08 -0500

    alter comments

--- single.php
+++ single.php
@@ -1,32 +1,32 @@
 <?php get_header(); ?>
 
-<!-- main -->
-<main role="main" class="container">
-  <div class="row">
-    <div class="col-md-9">
-      <header>
-        <h1><?php the_title(); ?></h1>
-        <?php get_template_part('assets/inc/datetime'); ?>
-      </header>
+<main role="main">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <header>
+          <h1><?php the_title(); ?></h1>
+          <?php get_template_part('assets/inc/datetime'); ?>
+        </header>
 
-      <?php the_content(); ?>
+        <?php the_content(); ?>
 
-      <div>
-        <?php previous_post(); ?>
-        <?php next_post(); ?>
-      </div>
+        <div>
+          <?php previous_post(); ?>
+          <?php next_post(); ?>
+        </div>
 
-      <footer>
-        <?php comments_template(); ?>
-      </footer>
+        <footer>
+          <?php comments_template(); ?>
+        </footer>
 
-      <?php edit_post_link(); ?>
-    </div>
-    <div class="col-md-3">
-      <?php get_sidebar(); ?>
-    </div>
-  </div>
-</main>
-<!-- /main -->
+        <?php edit_post_link(); ?>
+      </div> <!-- /.col-md-9 -->
+      <div class="col-md-3">
+        <?php get_sidebar(); ?>
+      </div> <!-- /.col-md-3 -->
+    </div> <!-- /.row -->
+  </div> <!-- /.container -->
+</main> <!-- /main -->
 
 <?php get_footer(); ?>
