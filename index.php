2991224187295f58e66941b3062dc859dd1d6e2a
Stephen <stephenherko@gmail.com>
2015-05-06 23:26:39 -0500

    alter comments

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
 
-<!-- main -->
-<main class="container">
-  <div class="row">
-    <div class="col-md-9">
-      <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post(); ?>
 
-        <article>
-          <header>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <?php get_template_part('assets/inc/datetime'); ?>
-          </header>
+          <article>
+            <header>
+              <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+              <?php get_template_part('assets/inc/datetime'); ?>
+            </header>
 
-        </article>
+          </article>
 
-        <?php endwhile; ?>
-      <?php endif; ?>
-    </div>
-    <div class="col-md-3">
-      <?php get_sidebar(); ?>
-    </div>
-  </div>
-</main>
-<!-- /main -->
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div> <!-- /.col-md-9 -->
+      <div class="col-md-3">
+        <?php get_sidebar(); ?>
+      </div> <!-- /.col-md-3 -->
+    </div> <!-- /.row -->
+  </div> <!-- /.container -->
+</main> <!-- /main -->
 
 <?php get_footer(); ?>
