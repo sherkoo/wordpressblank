2b33e27df7edc416d56c35fbec6e86720ab61623
Stephen <stephenherko@gmail.com>
2015-05-06 23:40:19 -0500

    custom post formats

--- home.php
+++ home.php
@@ -6,26 +6,35 @@
       <div class="col-md-9">
         <?php if(have_posts()) : ?>
           <?php while(have_posts()) : the_post(); ?>
-            <article class="panel panel-default">
-              <div class="panel-body">
-                <header class="panel-header">
-                  <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
-                  <?php get_template_part('assets/inc/datetime'); ?>
-                </header>
+            
+            <? if(has_post_format('gallery')) : ?>
+              <?php echo 'this is the video format'; ?>
+            <?php elseif(has_post_format('aside')) : ?>
+              <h2><?php the_title(); ?></h2>
+              <small>For aside</small>
+            <?php else: ?>
+              <article class="panel panel-default">
+                <div class="panel-body">
+                  <header class="panel-header">
+                    <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php get_template_part('assets/inc/datetime'); ?>
+                  </header>
 
-                <!-- post thumbnail -->
-                <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-                  <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                    <?php the_post_thumbnail(array(200,200)); ?>
-                  </a>
-                <?php endif; ?>
-                <!-- /post thumbnail -->
+                  <!-- post thumbnail -->
+                  <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                      <?php the_post_thumbnail(array(200,200)); ?>
+                    </a>
+                  <?php endif; ?>
+                  <!-- /post thumbnail -->
 
-                <?php the_content(); ?>
-                <?php edit_post_link(); ?>
-              </div> <!-- /.panel-body -->
+                  <?php the_content(); ?>
+                  <?php edit_post_link(); ?>
+                </div> <!-- /.panel-body -->
 
-            </article>
+              </article>
+            <?php endif; ?>
+
           <?php endwhile; ?>
         <?php endif; ?>
       </div> <!-- /.col-md-9 -->
